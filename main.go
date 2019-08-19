f5fd2a1e23fc151748b6abddcd8af02599f95395
Ben McRae <bmcrae@me.com>
2019-08-18 17:54:30 -0700

    latest wiki update

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
+// Page docs
 type Page struct {
 	Title string
 	Body  []byte
-}
-
-func main() {
-	println("Hello")
 }
 
 func (p *Page) save() error {
@@ -18,8 +18,22 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
